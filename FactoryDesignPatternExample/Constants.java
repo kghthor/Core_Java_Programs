81f0803f65b6d8679493821ee7883e817b053619
Harish K G 💻🚀 <86233829+kghthor@users.noreply.github.com>
2024-04-22 14:57:08 +0530

    Update Constants.java

--- FactoryDesignPatternExample/Constants.java
+++ FactoryDesignPatternExample/Constants.java
@@ -3,6 +3,6 @@ package FactoryDesignPatternExample;
 public class Constants {
     public final static String BIG_CINEMA = "bigcinema";
     public final static String INOX_CINEMA = "inoxcinema";
-    public final static String PVR_CINEMA = "pvrcinema";
+    public final static String PVR_CINEMA = "pvrcinema"; 
 
 }
