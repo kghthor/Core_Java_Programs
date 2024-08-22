f008e58e9b47b817f5ea76d7e343b2dcaa92b6d2
Harish K G 💻🚀 <86233829+kghthor@users.noreply.github.com>
2024-08-22 11:06:43 +0530

    Update InheritanceDemo.java

--- InheritanceProject1/InheritanceDemo.java
+++ InheritanceProject1/InheritanceDemo.java
@@ -5,7 +5,7 @@ public class InheritanceDemo {
         System.out.println("InheritanceDemo.InheritanceDemo()");
     }
 
-    public static void main(String args[]) {
+    public static void main(String args[]) { 
         System.out.println("InheritanceDemo.main()");
         CActive cActive = new CActive();
         cActive.task1();
